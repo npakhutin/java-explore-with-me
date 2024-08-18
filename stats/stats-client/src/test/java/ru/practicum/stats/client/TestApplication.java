30466a3386809af326051fdd3b42403b8179ab54
npakhutin <npakhutin@live.ru>
2024-08-18 20:34:34 +0300

    Добавлен код для фиктивного контейнера ewm-service

--- stats/stats-client/src/test/java/ru/practicum/stats/client/TestApplication.java
+++ stats/stats-client/src/test/java/ru/practicum/stats/client/TestApplication.java
@@ -3,4 +3,5 @@ package ru.practicum.stats.client;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestApplication {}
+public class TestApplication {
+}
